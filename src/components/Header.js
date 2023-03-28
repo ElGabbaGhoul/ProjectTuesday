ab334876be8298a484b714a2ae98cd6eac242c05
Marble <scnideffer@gmail.com>
2023-03-28 13:44:08 -0700

    added comments in header js for event handlers

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,23 @@ function Header() {
     <div className='LeftHeader'>
       <ul>
         <li>
+        {/* OnClick EventHandler for show about me */}
         <a href="/#/"><img src='./images/profile_icon.svg' alt='profile icon'>
         </img>
         <p>Home</p></a>
         </li>
         <li>
-        <a href="/#/section1"><img src='./images/resume_icon.svg' alt='profile icon'></img>
+        {/* OnClick EventHandler for show Resume */}
+        <a href="/#/section1"><img src='./images/resume_icon.svg' alt='resume icon'></img>
         <p>Resume</p></a>
         </li>
         <li>
-        <a href="/#/section2"><img src='./images/works_icon.svg' alt='profile icon'></img>
+        {/* OnClick EventHandler for show works */}
+        <a href="/#/section2"><img src='./images/works_icon.svg' alt='works icon'></img>
         <p>Works</p></a>
         </li>
         <li>
+        {/* OnClick EventHandler for show contact page */}
         <a href="/#/section3"><img src='./images/contact_icon.svg' alt='profile icon'></img>
         <p>Contact</p></a>
         </li>
